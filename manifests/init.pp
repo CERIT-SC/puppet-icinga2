519a307dcd1f62e5cf9bef0b51880ca02914674e
Marek Pastierik <marekpastierik15@gmail.com>
2019-02-13 11:00:26 +0100

    remove example of service

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,9 @@
 class icinga2 {
 
-  icinga2::host{ $fqdn: 
-    hostgroup     => "TEST-PUPPET",
-    check_command => "check_vzdy_ok",
-    templates     => ["generic-host"],
-    vars          => {"vars.tmp" => "asd" , "vars.example" => "asdsad" },
+  icinga2::host{ $fatcs['fqdn']: 
+    check_command        => "hostalive",
+    address              => $fatcs['ipaddress'],
+    templates            => ["generic-host"],
     enable_notifications => true,
-  }
-
-
-  icinga2::service{ "testService-API2":
-    enable_notifications     => true,
-    notification_user_groups => ["icingaadmins"],
-    notification_templates   => ["mail-service-notification"],
   }
 }
