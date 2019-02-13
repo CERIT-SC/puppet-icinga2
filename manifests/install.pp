06b4231292a71e4be233452d7f5f710bbdf7d682
Marek Pastierik <marekpastierik15@gmail.com>
2019-02-13 14:02:21 +0100

    remove package resource

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,2 @@
 class icinga2::install {
-  package { 'Resource rest-client on the puppetserver':
-    ensure   => "latest",
-    name     => 'rest-client',
-    provider => puppetserver_gem,
-  }
 }
