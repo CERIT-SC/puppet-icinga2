ce3df9cf78418e82e1b6e66f9b1bea04e2300a26
Marek Pastierik <marekpastierik15@gmail.com>
2019-02-19 15:38:00 +0100

    install packages

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,9 +1,14 @@
 class icinga2::install {
 
   $_allowed_hosts = lookup('icinga2::allowed_hosts', Array[String], 'first', ['127.0.0.1'])
+  $_packages      = lookup('icinga2::packages', Array[String], 'first', [])
   
   class { 'nrpe':
     allowed_hosts => $_allowed_hosts,
     server_port   => 5669,
+  }
+
+  $_packages.each |$name| {
+    package { $name: }
   }
 }
