0330036b4a2a105442dbedbe20cf701c89bcea1c
Marek Pastierik <marekpastierik15@gmail.com>
2019-02-20 10:59:17 +0100

    Add class for installing rest-client on server

---
+++ manifests/server.pp
@@ -0,0 +1,25 @@
+# Install dependencies onto the server
+#
+# @summary  This class will install the Rest-client gem onto the server and perform
+#           a reboot of the server.
+#
+# @example Declaring the class
+#   include icinga2::server
+#
+# @param [String] rest_client_version A specific release version of Rest client to install
+# @param [Type[Resource]] puppetserver_service  The name of the puppetserver service to reboot
+class icinga2::server(
+  String $rest_client_version = '1.8.0',
+  Type[Resource] $puppetserver_service = $facts['pe_server_version'] ? {
+    /./     => Service['pe-puppetserver'],
+    default => Service['puppetserver']
+  },
+) {
+    package { 'Rest client on the puppetserver':
+      ensure   => $rest_client_version,
+      name     => 'rest-client',
+      provider => puppetserver_gem,
+    }
+
+    Package['Rest client on the puppetserver'] ~> $puppetserver_service
+}
