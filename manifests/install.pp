97a404ccab220c08cfe770eed105521f55a13ffc
Marek Pastierik <marekpastierik15@gmail.com>
2019-02-21 14:23:07 +0100

    fix nrpe_plugins

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,12 +1,14 @@
 class icinga2::install {
 
-  $_allowed_hosts = lookup('icinga2::allowed_hosts', Array[String], 'first', ['127.0.0.1'])
-  $_packages      = lookup('icinga2::packages', Array[String], 'first', [])
-  
+  $_allowed_hosts           = lookup('icinga2::allowed_hosts', Array[String], 'first', ['127.0.0.1'])
+
   class { 'nrpe':
-    allowed_hosts => $_allowed_hosts,
-    server_port   => 5669,
+    allowed_hosts           => $_allowed_hosts,
+    server_port             => 5669,
+    nagios_plugins_packages => lookup('icinga2::nagios_plugins_package', Variant[Array[String],String], 'first', 'USE_DEFAULTS'), 
   }
+
+  $_packages = lookup('icinga2::packages', Array[String], 'first', [])
 
   $_packages.each |$name| {
     package { $name: }
