88e4966b79fa82631cc97f136ae934e355cf9611
Marek <marekpastierik15@gmail.com>
2019-04-10 13:09:58 +0200

    MODULE icinga2: add wrapper

---
+++ manifests/api.pp
@@ -0,0 +1,13 @@
+class icinga2::api (
+  String $user,
+  String $password,
+  String $url,
+) {
+     $_last_char = inline_template('<%= @url[-1,1] %>')
+
+     if $_last_char == "/" {
+        $new_url = regsubst($url, "^(.*):\/\/(.*)", "\1://${user}:${password}@\2v1/objects/")
+     } else {
+        $new_url = regsubst($url, "^(.*):\/\/(.*)", "\1://${user}:${password}@\2/v1/objects/")
+     }
+}
