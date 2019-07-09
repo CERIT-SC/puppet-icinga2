9afac2cf42bfeb8c91c525cee99f24a960d73e7a
marek130 <marekpastierik15@gmail.com>
2019-07-09 19:54:33 +0200

    MODULE icinga2: comment new provider for service

--- manifests/icinga2_service.pp
+++ manifests/icinga2_service.pp
@@ -10,51 +10,51 @@ define icinga2::icinga2_service (
    Optional[Array]   $notification_users       = [],
    Optional[Array]   $notification_templates   = [],
 ) {
-    $_attributes_to_set = [
-                            "check_command", "templates", "vars",
-                            "check_interval", "retry_interval", "check_timeout",
-                            "enable_notifications", "notification_user_groups", "display_name",
-                            "notification_users", "notification_templates", 
-                          ]
+#    $_attributes_to_set = [
+#                            "check_command", "templates", "vars",
+#                            "check_interval", "retry_interval", "check_timeout",
+#                            "enable_notifications", "notification_user_groups", "display_name",
+#                            "notification_users", "notification_templates", 
+#                          ]
 
-    $_attributes_to_set.each |$_key| {
-      ensure_resource('icinga2::service_res', $_key, {})
-    }
+#    $_attributes_to_set.each |$_key| {
+#      ensure_resource('icinga2::service_res', $_key, {})
+#    }
 
-    $_name_of_service = "${::fqdn}!${title}"
+#    $_name_of_service = "${::fqdn}!${title}"
 
-    icinga2_service { $_name_of_service:
-      check_command            => $check_command,
-      templates                => $templates,
-      vars                     => $vars,
-      display_name             => $title,
-      check_interval           => $check_interval,
-      retry_interval           => $retry_interval,
-      check_timeout            => $check_timeout,
-      enable_notifications     => $enable_notifications,
-      notification_user_groups => $notification_user_groups,
-      notification_users       => $notification_users,
-      notification_templates   => $notification_templates,
-      require                  => File['icinga2_url'],
-    }
+#    icinga2_service { $_name_of_service:
+#      check_command            => $check_command,
+#      templates                => $templates,
+#      vars                     => $vars,
+#      display_name             => $title,
+#      check_interval           => $check_interval,
+#      retry_interval           => $retry_interval,
+#      check_timeout            => $check_timeout,
+#      enable_notifications     => $enable_notifications,
+#      notification_user_groups => $notification_user_groups,
+#      notification_users       => $notification_users,
+#      notification_templates   => $notification_templates,
+#      require                  => File['icinga2_url'],
+#    }
 
-#     $_new_url    = $::icinga2::api::new_url
-#     $_argumments = { "attrs" => {
-#                       "vars"                 => $vars,
-#                       "check_command"        => $check_command,
-#                       "check_interval"       => $check_interval,
-#                       "retry_interval"       => $retry_interval,
-#                       "check_timeout"        => $check_timeout,
-#                       "enable_notifications" => $enable_notifications,
-#                    },
-#     }
-#
-#     $_filtered_argumments = $_argumments['attrs'].filter |$k, $v| { $v != undef }
-#     $arguments = { "hostname" => $facts['fqdn'], "attrs" => $_filtered_argumments, "templates" => $templates,
-#                    "notification" => { "user_groups" => $notification_user_groups, "users" => $notification_users },
-#                    "notification_templates" => $notification_templates
-#                  }
-#
-#     icinga2::create_object($title, "service", $arguments, $_new_url)
+     $_new_url    = $::icinga2::api::new_url
+     $_argumments = { "attrs" => {
+                       "vars"                 => $vars,
+                       "check_command"        => $check_command,
+                       "check_interval"       => $check_interval,
+                       "retry_interval"       => $retry_interval,
+                       "check_timeout"        => $check_timeout,
+                       "enable_notifications" => $enable_notifications,
+                    },
+     }
+
+     $_filtered_argumments = $_argumments['attrs'].filter |$k, $v| { $v != undef }
+     $arguments = { "hostname" => $facts['fqdn'], "attrs" => $_filtered_argumments, "templates" => $templates,
+                    "notification" => { "user_groups" => $notification_user_groups, "users" => $notification_users },
+                    "notification_templates" => $notification_templates
+                  }
+
+     icinga2::create_object($title, "service", $arguments, $_new_url)
 }
 
