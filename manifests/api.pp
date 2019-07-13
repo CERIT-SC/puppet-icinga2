fb629d97feadbe5b0385bf125cbee7bb43db8a4d
marek130 <marekpastierik15@gmail.com>
2019-07-13 13:36:03 +0200

    MODULE icinga2: fix

--- manifests/api.pp
+++ manifests/api.pp
@@ -3,15 +3,14 @@ class icinga2::api (
   Array[String] $passwords,
   Array[String] $urls,
 ) {
-     $users.map |Integer $index, String $user| {
+     $_new_urls = $users.map |Integer $index, String $user| {
          $_last_char = inline_template('<%= @urls[@index][-1,1] %>')
 
          if $_last_char == "/" {
-            regsubst($urls[$index], "^(.*):\/\/(.*)", "\1://${users[$index]}:${passwords[$index]}@\2v1/objects/")
+            $_tmp_url = regsubst($urls[$index], "^(.*):\/\/(.*)", "\1://${users[$index]}:${passwords[$index]}@\2v1/objects/")
          } else {
-            regsubst($urls[$index], "^(.*):\/\/(.*)", "\1://${users[$index]}:${passwords[$index]}@\2/v1/objects/")
+            $_tmp_url = regsubst($urls[$index], "^(.*):\/\/(.*)", "\1://${users[$index]}:${passwords[$index]}@\2/v1/objects/")
          }
+         $_tmp_url
      }
-     
-     $_new_urls = $users
 }
