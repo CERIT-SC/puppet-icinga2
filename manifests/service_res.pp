e3fb4144f8e220a185ccd876672fb9b2985225ea
marek130 <marekpastierik15@gmail.com>
2019-07-30 15:14:25 +0200

    MODULE icinga2: fragment requires concat

--- manifests/service_res.pp
+++ manifests/service_res.pp
@@ -3,6 +3,7 @@ define icinga2::service_res (
   concat::fragment { "icinga2_service_${title}":
     target  => "/var/tmp/icinga2_service_resources",
     content => "${title}\n",
+    require => Concat['/var/tmp/icinga2_service_resources']
   }
 }
 
