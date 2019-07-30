19c6d769a7128d0672d63e6a64c840310f2deb1c
marek130 <marekpastierik15@gmail.com>
2019-07-30 15:24:03 +0200

    MODULE icinga2: add a relationship

--- manifests/host_res.pp
+++ manifests/host_res.pp
@@ -3,5 +3,6 @@ define icinga2::host_res (
   concat::fragment { "icinga2_host_${title}":
     target  => "/var/tmp/icinga2_host_resources",
     content => "${title}\n",
+    require => Concat['/var/tmp/icinga2_host_resources'],
   }
 } 
