082f62749f1cd1fda3297075784f286e7c9d4623
marek130 <marekpastierik15@gmail.com>
2019-07-30 15:25:41 +0200

    MODULE icinga2: add installation resource_api for server

--- manifests/server.pp
+++ manifests/server.pp
@@ -10,6 +10,7 @@
 # @param [Type[Resource]] puppetserver_service  The name of the puppetserver service to reboot
 class icinga2::server(
   String $rest_client_version = '1.8.0',
+  String $api_version         = 'latest',
   Type[Resource] $puppetserver_service = $facts['pe_server_version'] ? {
     /./     => Service['pe-puppetserver'],
     default => Service['puppetserver']
@@ -18,6 +19,12 @@ class icinga2::server(
     package { 'Rest client on the puppetserver':
       ensure   => $rest_client_version,
       name     => 'rest-client',
+      provider => puppetserver_gem,
+    }
+    
+    package { 'Resource API on the puppetserver':
+      ensure   => $api_version,
+      name     => 'puppet-resource_api',
       provider => puppetserver_gem,
     }
 
